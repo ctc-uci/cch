29a27cc6ee6f348e8864139f17b28abbd26ed75a
alexy-ok <alexanderyin5280@gmail.com>
2024-11-24 15:49:42 -0800

    first commit

---
+++ server/db/schema/exit_survey.sql
@@ -0,0 +1,24 @@
+CREATE TYPE rating AS ENUM ("Excellent", "Good", "Fair", "Unsatisfactory");
+CREATE TYPE rating_help as ENUM("very helpful", "helpful", "not very helpful", "not helpful at all");
+
+CREATE TABLE exit_survey (
+    id integer PRIMARY KEY,
+    cm_id integer NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    site VARCHAR(32) NOT NULL,
+    program_date_completion DATE NOT NULL,
+    cch_rating rating NOT NULL,
+    cch_like_most VARCHAR(2048) NOT NULL,
+    life_skills_rating rating_help NOT NULL,
+    life_skills_helpful_topics VARCHAR(2048) NOT NULL,
+    life_skills_offer_topics_in_the_future VARCHAR(2048) NOT NULL,
+    cm_rating rating_help NOT NULL,
+    cm_change_about VARCHAR(2048) NOT NULL,
+    cm_most_beneficial VARCHAR(2048) NOT NULL,
+    experience_takeaway VARCHAR(2048) NOT NULL,
+    experience_accomplished VARCHAR(2048) NOT NULL,
+    experience_extra_notes VARCHAR(2048) NOT NULL,
+
+    FOREIGN KEY(cm_id) REFERENCES case_manager(id),
+    FOREIGN KEY(site) REFERENCES locations(id)
+);
