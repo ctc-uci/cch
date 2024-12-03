707fb9d923726729a4a1fbb8e63819882823187f
IsaacNguyen <isaachugh33@gmail.com>
2024-12-03 11:42:44 -0800

    made random survey table

---
+++ server/db/schema/random_survey_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE random_survey_table (
+    id integer PRIMARY KEY NOT NULL,    
+    date DATE NOT NULL,
+    cch_qos integer NOT NULL,
+    cm_qos integer NOT NULL,
+    courteous boolean NOT NULL,
+    informative boolean NOT NULL,
+    prompt_and_helpful boolean NOT NULL,
+    entry_quality integer NOT NULL,
+    unit_quality integer NOT NULL,
+    clean integer NOT NULL,
+    overall_experience integer NOT NULL,
+    case_meeting_frequency integer NOT NULL,
+    lifeskills boolean NOT NULL,
+    recommend boolean NOT NULL,
+    recommend_reasoning varchar(2048) NOT NULL,
+    make_cch_more_helpful varchar(2048) NOT NULL,
+    cm_id varchar(256) NOT NULL,
+    cm_feedback varchar(2048) NOT NULL,
+    other_comments varchar(2048),
+
+    FOREIGN KEY(cm_id) REFERENCES case_manager(id)
+);
