7394de2effb511a7d1ac0d7528cca28ae888f301
Sean Kelman <seanrkelman@gmail.com>
2024-12-05 14:43:43 -0800

    updated some column types and added client_status enum

--- server/db/schema/clients.sql
+++ server/db/schema/clients.sql
@@ -2,21 +2,22 @@ DROP TABLE IF EXISTS clients;
 
 CREATE TYPE ethnicity AS ENUM ('Non-Hispanic', 'Hispanic', 'Refused');
 CREATE TYPE race AS ENUM('Caucasian', 'Hispanic', 'African American', 'Asian', 'Pacific Islander/Hawaiian', 'Native American', 'Multi/Other');
+CREATE TYPE client_status AS ENUM('Active', 'Exited');
 
 CREATE TABLE clients (
-    id VARCHAR(6) NOT NULL PRIMARY KEY,
-    created_by VARCHAR(256) NOT NULL,
+    id SERIAL NOT NULL PRIMARY KEY,
+    created_by INT NOT NULL,
     unit_id INT NOT NULL,
     "grant" VARCHAR(256) NOT NULL,
-    "status" VARCHAR(256) NOT NULL, 
+    "status" client_status NOT NULL, 
     first_name VARCHAR(16) NOT NULL,
     last_name VARCHAR(16) NOT NULL,
     date_of_birth DATE NOT NULL,
     age INT NOT NULL,
-    phone_number INT NOT NULL,
+    phone_number VARCHAR(10) NOT NULL,
     email VARCHAR(32) NOT NULL,
     emergency_contact_name VARCHAR(32) NOT NULL,
-    emergency_contact_phone_number INT NOT NULL,
+    emergency_contact_phone_number VARCHAR(10) NOT NULL,
     medical BOOLEAN NOT NULL,
     entrance_date DATE NOT NULL,
     estimated_exit_date DATE NOT NULL,
@@ -38,7 +39,7 @@ CREATE TABLE clients (
     reason_for_leaving VARCHAR(1024),
     specific_reason_for_leaving VARCHAR(1024),
     specific_destination VARCHAR(64),
-    savings_amount FLOAT,
+    savings_amount NUMERIC(10, 2),
     attending_school_upon_exit BOOLEAN,
     reunified BOOLEAN,
     successful_completion BOOLEAN,
