65b9aa084deedf7ffcf706869ba2ac385224dd91
Sean Kelman <seanrkelman@gmail.com>
2024-12-05 14:49:09 -0800

    updated pkey to serial

--- server/db/schema/children.sql
+++ server/db/schema/children.sql
@@ -1,9 +1,11 @@
-CREATE TABLE IF NOT EXISTS public.children (
+DROP TABLE IF EXISTS children CASCADE;
+
+CREATE TABLE IF NOT EXISTS children (
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(16) NOT NULL,
     last_name VARCHAR(16) NOT NULL,
-    id INT PRIMARY KEY,
     parent_id INT NOT NULL,
     date_of_birth DATE NOT NULL,
     reunified BOOL NOT NULL,
-    FOREIGN KEY (parent_id) REFERENCES public.clients(id)
+    FOREIGN KEY (parent_id) REFERENCES clients(id)
 );
