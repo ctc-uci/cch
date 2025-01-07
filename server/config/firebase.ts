3730eceba80d3bd214f83ec8ba81626818319293
alexy-ok <alexanderyin5280@gmail.com>
2025-01-06 21:06:49 -0800

    finished

--- server/config/firebase.ts
+++ server/config/firebase.ts
@@ -1,4 +1,4 @@
-import firebaseAdmin, { type ServiceAccount } from "firebase-admin";
+/*import firebaseAdmin, { type ServiceAccount } from "firebase-admin";
 
 // https://firebase.google.com/docs/admin/setup#initialize_the_sdk_in_non-google_environments
 import serviceAccount from "./firebase-adminsdk.json";
@@ -6,3 +6,4 @@ import serviceAccount from "./firebase-adminsdk.json";
 export const admin = firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount as ServiceAccount),
 });
+*/
