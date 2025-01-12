4e4acd756170d924a180cdd44eb368e3199282ab
Nathan Che <nathan.che2006@gmail.com>
2025-01-11 22:27:28 -0800

    partial matching added to GET, comments removed, optional PUT, and ID serial value fixed

--- server/routes/clients.ts
+++ server/routes/clients.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { keysToCamel } from "../common/utils";
-import { db } from "../db/db-pgp"; // TODO: replace this db with
+import { db } from "../db/db-pgp"; 
 
 export const clientsRouter = Router();
 
@@ -13,48 +13,48 @@ clientsRouter.get("/:page?/:filter?/:search?", async (req, res) => {
         const { page, filter, search } = req.query;
         let queryStr = `SELECT * FROM clients`
         
-        const stringSearch = "'" + String(search) + "'"
+        const stringSearch = "'%" + String(search) + "%'"
         
         if (search) {
-          queryStr = queryStr.concat(" ", `WHERE id::TEXT = ${stringSearch} 
-            OR created_by::TEXT = ${stringSearch} 
-            OR unit_id::TEXT = ${stringSearch} 
-            OR "grant"::TEXT = ${stringSearch} 
-            OR "status"::TEXT = ${stringSearch} 
-            OR first_name::TEXT = ${stringSearch} 
-            OR last_name::TEXT = ${stringSearch} 
-            OR date_of_birth::TEXT = ${stringSearch} 
-            OR age::TEXT = ${stringSearch} 
-            OR phone_number::TEXT = ${stringSearch} 
-            OR email::TEXT = ${stringSearch} 
-            OR emergency_contact_name::TEXT = ${stringSearch} 
-            OR emergency_contact_phone_number::TEXT = ${stringSearch} 
-            OR medical::TEXT = ${stringSearch} 
-            OR entrance_date::TEXT = ${stringSearch} 
-            OR estimated_exit_date::TEXT = ${stringSearch} 
-            OR exit_date::TEXT = ${stringSearch} 
-            OR bed_nights::TEXT = ${stringSearch} 
-            OR bed_nights_children::TEXT = ${stringSearch} 
-            OR pregnant_upon_entry::TEXT = ${stringSearch} 
-            OR disabled_children::TEXT = ${stringSearch} 
-            OR ethnicity::TEXT = ${stringSearch} 
-            OR race::TEXT = ${stringSearch} 
-            OR city_of_last_permanent_residence::TEXT = ${stringSearch} 
-            OR prior_living::TEXT = ${stringSearch} 
-            OR prior_living_city::TEXT = ${stringSearch} 
-            OR shelter_in_last_five_years::TEXT = ${stringSearch} 
-            OR homelessness_length::TEXT = ${stringSearch} 
-            OR chronically_homeless::TEXT = ${stringSearch} 
-            OR attending_school_upon_entry::TEXT = ${stringSearch} 
-            OR employement_gained::TEXT = ${stringSearch} 
-            OR reason_for_leaving::TEXT = ${stringSearch} 
-            OR specific_reason_for_leaving::TEXT = ${stringSearch} 
-            OR specific_destination::TEXT = ${stringSearch} 
-            OR savings_amount::TEXT = ${stringSearch} 
-            OR attending_school_upon_exit::TEXT = ${stringSearch} 
-            OR reunified::TEXT = ${stringSearch} 
-            OR successful_completion::TEXT = ${stringSearch} 
-            OR destination_city::TEXT = ${stringSearch}`)
+          queryStr = queryStr.concat(" ", `WHERE id::TEXT ILIKE ${stringSearch} 
+            OR created_by::TEXT ILIKE ${stringSearch} 
+            OR unit_id::TEXT ILIKE ${stringSearch} 
+            OR "grant"::TEXT ILIKE ${stringSearch} 
+            OR "status"::TEXT ILIKE ${stringSearch} 
+            OR first_name::TEXT ILIKE ${stringSearch} 
+            OR last_name::TEXT ILIKE ${stringSearch} 
+            OR date_of_birth::TEXT ILIKE ${stringSearch} 
+            OR age::TEXT ILIKE ${stringSearch} 
+            OR phone_number::TEXT ILIKE ${stringSearch} 
+            OR email::TEXT ILIKE ${stringSearch} 
+            OR emergency_contact_name::TEXT ILIKE ${stringSearch} 
+            OR emergency_contact_phone_number::TEXT ILIKE ${stringSearch} 
+            OR medical::TEXT ILIKE ${stringSearch} 
+            OR entrance_date::TEXT ILIKE ${stringSearch} 
+            OR estimated_exit_date::TEXT ILIKE ${stringSearch} 
+            OR exit_date::TEXT ILIKE ${stringSearch} 
+            OR bed_nights::TEXT ILIKE ${stringSearch} 
+            OR bed_nights_children::TEXT ILIKE ${stringSearch} 
+            OR pregnant_upon_entry::TEXT ILIKE ${stringSearch} 
+            OR disabled_children::TEXT ILIKE ${stringSearch} 
+            OR ethnicity::TEXT ILIKE ${stringSearch} 
+            OR race::TEXT ILIKE ${stringSearch} 
+            OR city_of_last_permanent_residence::TEXT ILIKE ${stringSearch} 
+            OR prior_living::TEXT ILIKE ${stringSearch} 
+            OR prior_living_city::TEXT ILIKE ${stringSearch} 
+            OR shelter_in_last_five_years::TEXT ILIKE ${stringSearch} 
+            OR homelessness_length::TEXT ILIKE ${stringSearch} 
+            OR chronically_homeless::TEXT ILIKE ${stringSearch} 
+            OR attending_school_upon_entry::TEXT ILIKE ${stringSearch} 
+            OR employement_gained::TEXT ILIKE ${stringSearch} 
+            OR reason_for_leaving::TEXT ILIKE ${stringSearch} 
+            OR specific_reason_for_leaving::TEXT ILIKE ${stringSearch} 
+            OR specific_destination::TEXT ILIKE ${stringSearch} 
+            OR savings_amount::TEXT ILIKE ${stringSearch} 
+            OR attending_school_upon_exit::TEXT ILIKE ${stringSearch} 
+            OR reunified::TEXT ILIKE ${stringSearch} 
+            OR successful_completion::TEXT ILIKE ${stringSearch} 
+            OR destination_city::TEXT ILIKE ${stringSearch}`)
         }
 
         queryStr += ' ORDER BY id ASC'
@@ -77,7 +77,7 @@ clientsRouter.post('/', async (req, res) => {
 
   try {
 
-      const { id, created_by, unit_id, grant, status, 
+      const { created_by, unit_id, grant, status, 
           first_name, last_name, date_of_birth, age,
           phone_number, email, emergency_contact_name,
           emergency_contact_phone_number, medical, entrance_date,
@@ -94,7 +94,6 @@ clientsRouter.post('/', async (req, res) => {
       const data = await db.query(
         
         `INSERT INTO clients (
-          id, 
           created_by, 
           unit_id, 
           "grant", 
@@ -135,13 +134,13 @@ clientsRouter.post('/', async (req, res) => {
           destination_city, 
         ) VALUES (
           $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $27, 
-          $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39
+          $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38
         )
         RETURNING id;
         `,
 
         [
-          id, created_by, unit_id, grant, status, 
+          created_by, unit_id, grant, status, 
           first_name, last_name, date_of_birth, age,
           phone_number, email, emergency_contact_name,
           emergency_contact_phone_number, medical, entrance_date,
@@ -169,82 +168,23 @@ clientsRouter.post('/', async (req, res) => {
 // Updates a client based on ID.
 clientsRouter.put("/:id", async (req, res) => {
 
+  console.log('put called')
+
   try {
 
-    const { created_by, unit_id, grant, status, 
-        first_name, last_name, date_of_birth, age,
-        phone_number, email, emergency_contact_name,
-        emergency_contact_phone_number, medical, entrance_date,
-        estimated_exit_date, exit_date, bed_nights, bed_nights_children,
-        pregnant_upon_entry, disabled_children, ethnicity, race,
-        city_of_last_permanent_residence, prior_living, prior_living_city,
-        shelter_in_last_five_years, homelessness_length, chronically_homeless,
-        attending_school_upon_entry, employement_gained, reason_for_leaving,
-        specific_reason_for_leaving, specific_destination,  savings_amount,
-        attending_school_upon_exit, reunified, successful_completion,
-        destination_city } = req.body;
-
+    const updateData = req.body;
     const { id } = req.params;
 
-    await db.query(
-      `UPDATE clients 
-      SET
-        id = $1
-        created_by = $2
-        unit_id = $3
-        "grant" = $4
-        "status" = $5
-        first_name = $6
-        last_name = $7
-        date_of_birth = $8 
-        age = $9
-        phone_number = $10 
-        email = $11
-        emergency_contact_name = $12
-        emergency_contact_phone_number = $13 
-        medical = $14
-        entrance_date = $15
-        estimated_exit_date = $16 
-        exit_date = $17
-        bed_nights = $18
-        bed_nights_children = $19
-        pregnant_upon_entry = $20
-        disabled_children = $21
-        ethnicity = $22
-        race = $23
-        city_of_last_permanent_residence = $24 
-        prior_living = $25
-        prior_living_city = $26
-        shelter_in_last_five_years = $27 
-        homelessness_length = $28
-        chronically_homeless = $29
-        attending_school_upon_entry = $30 
-        employement_gained = $31
-        reason_for_leaving = $32
-        specific_reason_for_leaving = $33 
-        specific_destination = $34
-        savings_amount = $35
-        attending_school_upon_exit = $36 
-        reunified = $37
-        successful_completion = $38
-        destination_city = $39
-      WHERE id=${id} 
-      `,
-        [
-          id, created_by, unit_id, grant, status, 
-          first_name, last_name, date_of_birth, age,
-          phone_number, email, emergency_contact_name,
-          emergency_contact_phone_number, medical, entrance_date,
-          estimated_exit_date, exit_date, bed_nights, bed_nights_children,
-          pregnant_upon_entry, disabled_children, ethnicity, race,
-          city_of_last_permanent_residence, prior_living, prior_living_city,
-          shelter_in_last_five_years, homelessness_length, chronically_homeless,
-          attending_school_upon_entry, employement_gained, reason_for_leaving,
-          specific_reason_for_leaving, specific_destination,  savings_amount,
-          attending_school_upon_exit, reunified, successful_completion,
-          destination_city 
-        ]
-    );
+    let query = `UPDATE clients SET `;
+
+    for (const [key, value] of Object.entries(updateData)) {
+      query += `${key} = ${value}, `;
+    }
+
+    query = query.slice(0, -2);
+    query += ` WHERE id = ${id}`;
+
+    await db.query(query);
 
     res.status(200).json(keysToCamel({id}));
 
