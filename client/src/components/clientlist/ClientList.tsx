164d6e15a461457e4c50a03df522cd48da4aee6d
Isaac Nguyen <isaachn@uci.edu>
2025-02-04 19:40:32 -0800

    filter

--- client/src/components/clientlist/ClientList.tsx
+++ client/src/components/clientlist/ClientList.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import {
   Button,
-  Input,
   Heading,
+  HStack,
+  Input,
   Table,
   TableContainer,
   Tbody,
@@ -14,11 +14,13 @@ import {
   Thead,
   Tr,
   VStack,
-  HStack,
 } from "@chakra-ui/react";
+
+import { useNavigate } from "react-router-dom";
 
 import { useAuthContext } from "../../contexts/hooks/useAuthContext";
 import { useBackendContext } from "../../contexts/hooks/useBackendContext";
+import { ClientListFilter } from "../clientlist/ClientListFilter";
 
 interface Client {
   id: string;
@@ -26,9 +28,41 @@ interface Client {
   lastName: string;
   phoneNumber: string;
   email: string;
+  caseManagerFirstName: string;
+  caseManagerLastName: string;
   entranceDate: string;
   exitDate: string;
   dateOfBirth: string;
+  bedNights: number;
+  bedNightsChildren: number;
+  createdBy: string;
+  grant: string;
+  age: number;
+  emergencyContactName: string;
+  emergencyContactPhoneNumber: string;
+  medical: boolean;
+  estimatedExitDate: string;
+  pregnantUponEntry: string;
+  disabledChildren: string;
+  ethnicity: string;
+  race: string;
+  cityOfLastPermanentResidence: string;
+  priorLiving: string;
+  priorLivingCity: string;
+  shelterInLastFiveYears: string;
+  homelessnessLength: number;
+  chronicallyHomeless: string;
+  attendingSchoolUponEntry: string;
+  employementGained: string;
+  reasonForLeaving: string;
+  specificReasonForLeaving: string;
+  specificDestination: string;
+  savingsAmount: string;
+  attendingSchoolUponExit: string;
+  reunified: string;
+  successfulCompletion: string;
+  destinationCity: string;
+  locationName: string;
 }
 
 export const ClientList = () => {
@@ -37,15 +71,24 @@ export const ClientList = () => {
 
   const [clients, setClients] = useState<Client[]>([]);
   const [searchKey, setSearchKey] = useState("");
+  const [filterQuery, setFilterQuery] = useState<string[]>([]);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        console.log(filterQuery)
         let response;
         if (searchKey) {
-          response = await backend.get(`/clients?page=&filter=&search=${searchKey}`);
+          response = await backend.get(
+            `/clients?page=&filter=&search=${searchKey}`
+          );
+        } else if (filterQuery.length > 1) {
+          console.log(filterQuery.join(""))
+          response = await backend.get(
+            `/clients?page=&filter=${encodeURIComponent(filterQuery.join(" "))}&search=`
+          );
         } else {
           response = await backend.get("/clients");
         }
@@ -55,34 +98,55 @@ export const ClientList = () => {
       }
     };
     fetchData();
-  }, [backend, searchKey]);
+  }, [backend, searchKey, filterQuery]);
 
   return (
     <VStack
-      spacing = {2}
-      align = "start"
+      spacing={2}
+      align="start"
       sx={{ maxWidth: "100%", marginX: "auto", padding: "4%" }}
     >
-      <Heading paddingBottom = "4%">Welcome, {currentUser?.displayName}</Heading>
+      <Heading paddingBottom="4%">Welcome, {currentUser?.displayName}</Heading>
       <HStack width="100%">
-        <Heading size = "md">My Complete Client Table</Heading>
-        <Heading size = "sm" paddingLeft = "10%">Last Updated: {}</Heading>
+        <Heading size="md">My Complete Client Table</Heading>
+        <Heading
+          size="sm"
+          paddingLeft="10%"
+        >
+          Last Updated: {}
+        </Heading>
       </HStack>
 
-      <VStack>
-      </VStack>
-      <HStack width = "100%" justifyContent = "space-between">
-        <Input fontSize = "12px" width = "20%" height= "30px" placeholder = 'search' onChange={(e) => setSearchKey(e.target.value)}/>
-        <HStack width  = "55%" justifyContent="space-between">
-          <Text fontSize = "12px">showing {Object.keys(clients).length} results on this page</Text>
+      <VStack></VStack>
+      <HStack
+        width="100%"
+        justifyContent="space-between"
+      >
+        <Input
+          fontSize="12px"
+          width="20%"
+          height="30px"
+          placeholder="search"
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <ClientListFilter setFilterQuery={setFilterQuery}/>
+        <HStack
+          width="55%"
+          justifyContent="space-between"
+        >
+          <Text fontSize="12px">
+            showing {Object.keys(clients).length} results on this page
+          </Text>
           <HStack>
             <Button></Button>
-            <Text fontSize = "12px">page {} of {Math.ceil((Object.keys(clients).length)/20)}</Text>
+            <Text fontSize="12px">
+              page {} of {Math.ceil(Object.keys(clients).length / 20)}
+            </Text>
             <Button></Button>
           </HStack>
           <HStack>
-            <Button fontSize = "12px">delete</Button>
-            <Button fontSize = "12px">add</Button>
+            <Button fontSize="12px">delete</Button>
+            <Button fontSize="12px">add</Button>
           </HStack>
         </HStack>
       </HStack>
@@ -90,7 +154,7 @@ export const ClientList = () => {
         sx={{
           overflowX: "auto",
           maxWidth: "100%",
-          border: "1px solid gray"
+          border: "1px solid gray",
         }}
       >
         <Table variant="striped">
@@ -98,24 +162,88 @@ export const ClientList = () => {
             <Tr>
               <Th>Client First Name</Th>
               <Th>Client Last Name</Th>
-              <Th>Phone Number</Th>
-              <Th>E-mail</Th>
-              <Th>Entrance Date</Th>
-              <Th>Exit Date</Th>
+              <Th>Case Manager</Th>
+              <Th>Site</Th>
+              <Th>Grant</Th>
               <Th>Birthday</Th>
+              <Th>Age</Th>
+              <Th>Entry Date</Th>
+              <Th>Exit Date</Th>
+              <Th>Bed Nights</Th>
+              <Th>Total Bed Nights w/ Children</Th>
+              <Th>Phone Number</Th>
+              <Th>Email</Th>
+              <Th>Emergency Contact Name</Th>
+              <Th>Emergency Contact Phone</Th>
+              <Th>Medical</Th>
+              <Th>Estimated Exit Date</Th>
+              <Th>Pregnant Upon Entry</Th>
+              <Th>Disabled Children</Th>
+              <Th>Ethnicity</Th>
+              <Th>Race</Th>
+              <Th>City of last Permanent Residence</Th>
+              <Th>Prior Living</Th>
+              <Th>Prior Living City</Th>
+              <Th>Shelter in Last Five Years</Th>
+              <Th>Homelessness Length</Th>
+              <Th>Chronically Homelessness</Th>
+              <Th>Attending School Upon Entry</Th>
+              <Th>Employment Gained</Th>
+              <Th>Reason For Leaving</Th>
+              <Th>Specific Reason for Leaving</Th>
+              <Th>Specific Destination</Th>
+              <Th>Savings Amount</Th>
+              <Th>Attending School Upon Exit</Th>
+              <Th>Reunified</Th>
+              <Th>Successful Completion</Th>
+              <Th>Destination City</Th>
             </Tr>
           </Thead>
           <Tbody>
             {clients
               ? clients.map((client) => (
-                  <Tr key={client.id} onClick={() => navigate(``)} style={{ cursor: "pointer" }}>
+                  <Tr
+                    key={client.id}
+                    onClick={() => navigate(``)}
+                    style={{ cursor: "pointer" }}
+                  >
                     <Td>{client.firstName}</Td>
                     <Td>{client.lastName}</Td>
-                    <Td>{client.phoneNumber}</Td>
-                    <Td>{client.email}</Td>
+                    <Td>{client.caseManagerFirstName} {client.caseManagerLastName}</Td>
+                    <Td>{client.locationName}</Td>
+                    <Td>{client.grant}</Td>
+                    <Td>{client.dateOfBirth}</Td>
+                    <Td>{client.age}</Td>
                     <Td>{client.entranceDate}</Td>
                     <Td>{client.exitDate}</Td>
-                    <Td>{client.dateOfBirth}</Td>
+                    <Td>{client.bedNights}</Td>
+                    <Td>{client.bedNightsChildren}</Td>
+                    <Td>{client.phoneNumber}</Td>
+                    <Td>{client.email}</Td>
+                    <Td>{client.emergencyContactName}</Td>
+                    <Td>{client.emergencyContactPhoneNumber}</Td>
+                    <Td>{client.medical ? "Yes" : "No"}</Td>
+                    <Td>{client.estimatedExitDate}</Td>
+                    <Td>{client.pregnantUponEntry ? "Yes" : "No"}</Td>
+                    <Td>{client.disabledChildren ? "Yes" : "No"}</Td>
+                    <Td>{client.ethnicity}</Td>
+                    <Td>{client.race}</Td>
+                    <Td>{client.cityOfLastPermanentResidence}</Td>
+                    <Td>{client.priorLiving}</Td>
+                    <Td>{client.priorLivingCity}</Td>
+                    <Td>{client.shelterInLastFiveYears ? "Yes" : "No"}</Td>
+                    <Td>{client.homelessnessLength}</Td>
+                    <Td>{client.chronicallyHomeless ? "Yes" : "No"}</Td>
+                    <Td>{client.attendingSchoolUponEntry ? "Yes" : "No"}</Td>
+                    <Td>{client.employementGained ? "Yes" : "No"}</Td>
+                    <Td>{client.reasonForLeaving}</Td>
+                    <Td>{client.specificReasonForLeaving}</Td>
+                    <Td>{client.specificDestination}</Td>
+                    <Td>{client.savingsAmount}</Td>
+                    <Td>{client.attendingSchoolUponExit ? "Yes" : "No"}</Td>
+                    <Td>{client.reunified ? "Yes" : "No"}</Td>
+                    <Td>{client.successfulCompletion ? "Yes" : "No"}</Td>
+                    <Td>{client.destinationCity}</Td>
                   </Tr>
                 ))
               : null}
---
+++ client/src/components/clientlist/ClientListFilter.tsx
@@ -0,0 +1,334 @@
+import { useEffect, useState } from "react";
+
+import {
+  Box,
+  Button,
+  HStack,
+  Icon,
+  Input,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Select,
+  Text,
+  VStack,
+  useToast 
+} from "@chakra-ui/react";
+
+import {
+  MdOutlineAdd,
+  MdOutlineDelete,
+  MdOutlineFilterAlt,
+} from "react-icons/md";
+
+interface ClientListFilterProps {
+  setFilterQuery: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+
+export const ClientListFilter = ({setFilterQuery}: ClientListFilterProps) => {
+    const toast = useToast();
+    const [nextId, setNextId] = useState(2);
+
+    const columns = [
+      { name: "Client First Name", value: "clients.first_name", type: "string" },
+      { name: "Client Last Name", value: "clients.last_name", type: "string" },
+      { name: "Date of Birth", value: "clients.date_of_birth", type: "string" },
+      { name: "Case Manager First Name", value: "case_managers.first_name", type: "string" },
+      { name: "Case Manager Last Name", value: "case_managers.last_name", type: "string" },
+      { name: "Location", value: "locations.name", type: "string" },
+      { name: "Unit ID", value: "clients.unit_id", type: "number" },
+      { name: "Grant", value: "clients.grant", type: "string" },
+      { name: "Status", value: "clients.status", type: "string" },
+      { name: "Age", value: "clients.age", type: "number" },
+      { name: "Phone Number", value: "clients.phone_number", type: "string" },
+      { name: "Email", value: "clients.email", type: "string" },
+      { name: "Emergency Contact Name", value: "clients.emergency_contact_name", type: "string" },
+      { name: "Emergency Contact Phone", value: "clients.emergency_contact_phone_number", type: "string" },
+      { name: "Medical", value: "clients.medical", type: "boolean" },
+      { name: "Entrance Date", value: "clients.entrance_date", type: "string" },
+      { name: "Estimated Exit Date", value: "clients.estimated_exit_date", type: "string" },
+      { name: "Exit Date", value: "clients.exit_date", type: "string" },
+      { name: "Bed Nights", value: "clients.bed_nights", type: "number" },
+      { name: "Bed Nights for Children", value: "clients.bed_nights_children", type: "number" },
+      { name: "Pregnant Upon Entry", value: "clients.pregnant_upon_entry", type: "boolean" },
+      { name: "Disabled Children", value: "clients.disabled_children", type: "boolean" },
+      { name: "Ethnicity", value: "clients.ethnicity", type: "string" },
+      { name: "Race", value: "clients.race", type: "string" },
+      { name: "City of Last Permanent Residence", value: "clients.city_of_last_permanent_residence", type: "string" },
+      { name: "Prior Living", value: "clients.prior_living", type: "string" },
+      { name: "Prior Living City", value: "clients.prior_living_city", type: "string" },
+      { name: "Shelter in Last 5 Years", value: "clients.shelter_in_last_five_years", type: "boolean" },
+      { name: "Homelessness Length", value: "clients.homelessness_length", type: "number" },
+      { name: "Chronically Homeless", value: "clients.chronically_homeless", type: "boolean" },
+      { name: "Attending School Upon Entry", value: "clients.attending_school_upon_entry", type: "boolean" },
+      { name: "Employment Gained", value: "clients.employement_gained", type: "boolean" },
+      { name: "Reason for Leaving", value: "clients.reason_for_leaving", type: "string" },
+      { name: "Specific Reason for Leaving", value: "clients.specific_reason_for_leaving", type: "string" },
+      { name: "Specific Destination", value: "clients.specific_destination", type: "string" },
+      { name: "Savings Amount", value: "clients.savings_amount", type: "number" },
+      { name: "Attending School Upon Exit", value: "clients.attending_school_upon_exit", type: "boolean" },
+      { name: "Reunified", value: "clients.reunified", type: "boolean" },
+      { name: "Successful Completion", value: "clients.successful_completion", type: "boolean" },
+      { name: "Destination City", value: "clients.destination_city", type: "string" },
+    ];
+    
+  
+
+  const [filterRows, setFilterRows] = useState([
+      {
+      id: 1,
+      selector: "",
+      field: "",
+      operator: "",
+      value: "",
+      },
+  ]);
+
+  useEffect(() => {
+    setFilterQuery([
+      "WHERE",
+      ...filterRows.map((row) => {
+        if (row.field !== "" && row.operator !== "" && row.value !== "") {
+          if (row.operator === "contains") {
+            return `${row.selector} ${row.field} ILIKE '%${row.value}%'`;
+          }
+          return `${row.selector} ${row.field} ${row.operator} '${row.value}'`;
+        }
+        return "";
+      })
+      .filter((query: string) => query !== ""),
+    ]);
+  }, [filterRows]);
+
+  const addNewRow = () => {
+    if (filterRows.length > 0 && filterRows[filterRows.length - 1]?.field === "" || filterRows[filterRows.length - 1]?.value === "" || filterRows[filterRows.length - 1]?.operator === "") {
+        toast({
+            title: "Incomplete Fields",
+            description: "Please fill in all fields before adding a new filter",
+            status: "warning",
+            duration: 3000,
+            isClosable: true,
+          });
+        return;
+    }
+    setFilterRows([
+      ...filterRows,
+      {
+        id: nextId,
+        selector: "AND",
+        field: "",
+        operator: "",
+        value: "",
+      },
+    ]);
+    setNextId(nextId + 1);
+    
+  };
+
+  const removeRow = (id: number) => {
+    
+    if (filterRows.length > 1) {
+      setFilterRows((prevRows) => {
+        const updatedRows = prevRows.filter(row => row.id !== id);
+
+        if (updatedRows.length === 0) {
+            return [
+                {
+                    id: 1,
+                    selector: "",
+                    field: "",
+                    operator: "",
+                    value: "",
+                },
+            ];
+        }
+
+        return updatedRows.map((row, index) => ({
+            ...row,
+            selector: index === 0 ? "" : row.selector,
+        }));
+    });
+      
+    }
+    else {
+        setFilterRows([
+            {
+            id: 1,
+            selector: "",
+            field: "",
+            operator: "",
+            value: "",
+            },
+        ]);
+    }
+  };
+
+  const updateFilterValue = (id: number, selectType: string, value: string) => {
+    setFilterRows(filterRows.map(row => {
+      if (row.id === id) {
+        return { ...row, [selectType]: value };
+      }
+      return row;
+    }));
+  };
+
+  return (
+    <Popover placement="bottom-start">
+      <PopoverTrigger>
+        <Button>
+          <HStack>
+            <Icon as={MdOutlineFilterAlt} />
+            <h1>Filter</h1>
+          </HStack>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        width="auto"
+        p={2}
+      >
+        <PopoverArrow />
+        <PopoverBody>
+          <VStack
+            spacing={4}
+            align="stretch"
+            w="100%"
+          >
+            <Text
+              fontSize="14px"
+              fontWeight="300"
+            >
+              In this view, show records
+            </Text>
+            {filterRows.map((row, index) => (
+              <Box
+                key={row.id}
+                width="100%"
+                overflow="visible"
+              >
+                <HStack
+                  spacing={0}
+                  align="center"
+                >
+                {index === 0 ? (
+                     <Text
+                     fontWeight="medium"
+                     fontSize="md"
+                     marginRight={"47px"}
+                   >
+                     Where
+                   </Text>
+                ) : (
+                    <Select
+                    value={row.selector}
+                    marginRight={"14px"}
+                    onChange={(e) => updateFilterValue(row.id, 'selector', e.target.value)}
+                    maxWidth={"80px"}
+                  >
+                     <option value="AND">and</option>
+                     <option value="OR">or</option>
+                  </Select>
+                )}
+                
+
+                  <Select
+                    placeholder="Select Field"
+                    minWidth="150px"
+                    value={row.field}
+                    onChange={(e) => updateFilterValue(row.id, 'field', e.target.value)}
+                    borderRightRadius="0"
+                  >
+                    {
+                        columns.map((column) => {
+                            return <option key={column.name} typeof={column.type} value={column.value}>{column.name}</option>
+                        })
+                    }
+                  </Select>
+                 
+                     {(() => {
+                        const selectedColumn = columns.find((col) => col.value === row.field);
+                        const fieldType: string = selectedColumn?.type || "string";
+                        let operators: { value: string; label: string }[] = [];
+
+                        if (fieldType === "number") {
+                          operators = [
+                            { value: "=", label: "equals" },
+                            { value: "!=", label: "not equals" },
+                            { value: ">", label: "greater than" },
+                            { value: "<", label: "less than" },
+                          ];
+                        } else if (fieldType === "boolean") {
+                          operators = [
+                            { value: "=", label: "is" },
+                            { value: "!=", label: "is not" },
+                          ];
+                        } else if (fieldType === "string") {
+                          operators = [
+                            { value: "contains", label: "contains" },
+                            { value: "=", label: "equals" },
+                          ];
+                        }
+                        
+                        return (
+                          <>
+                             <Select
+                              placeholder="Select Operator"
+                              minWidth="150px"
+                              value={row.operator}
+                              onChange={(e) => updateFilterValue(row.id, 'operator', e.target.value)}
+                              borderRadius="0"
+                            >
+                              {operators.map((op) => (
+                                  <option key={op.value} value={op.value}>
+                                    {op.label}
+                                  </option>
+                                ))}
+                              </Select>
+                              {fieldType === "boolean" ? (
+                              <Select
+                                minWidth="150px"
+                                value={row.value}
+                                onChange={(e) => updateFilterValue(row.id, "value", e.target.value)}
+                              >
+                                <option value="true">Yes</option>
+                                <option value="false">No</option>
+                              </Select>
+                            ) : (
+                              <Input
+                                placeholder="Enter a value"
+                                minWidth="150px"
+                                value={row.value}
+                                onChange={(e) => updateFilterValue(row.id, "value", e.target.value)}
+                                borderRadius="0"
+                              />
+                            )}
+                          </>
+                        )
+               
+                      })()}
+              
+                  <Button onClick={() => removeRow(row.id)} variant='outline' borderLeftRadius="0">
+                    <Icon
+                        as={MdOutlineDelete}
+                    />
+                  </Button>
+                  
+                </HStack>
+              </Box>
+            ))}
+            <Button
+              leftIcon={<MdOutlineAdd />}
+              onClick={addNewRow}
+              size="sm"
+              variant="ghost"
+            >
+              Add Filter
+            </Button>
+          </VStack>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+};
