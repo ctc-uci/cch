249e0abac5d685f17f2a5c7d4c7da6de1806d434
Lindsay Lim <limlindsayj@gmail.com>
2025-02-15 01:22:27 -0800

    tabs made, children cards, comments input

---
+++ client/src/components/clientPage/childrenCards.tsx
@@ -0,0 +1,26 @@
+import { SimpleGrid, Card, CardHeader, Heading, CardBody, Text, CardFooter } from '@chakra-ui/react'
+
+import {Children} from './ViewPage'
+
+interface ChildrenProps {
+    items: Children[];
+}
+
+function ChildrenCards({items} : ChildrenProps) {
+    return (
+        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+            {items.map((item: Children, index: number) => (
+                <Card key={index}>
+                    <CardHeader>
+                        <Heading>{item.firstName} {item.lastName}</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Text>{item.dateOfBirth}</Text>
+                    </CardBody>
+                </Card>
+            ))}
+        </SimpleGrid>
+    )
+}
+
+export default ChildrenCards;
---
+++ client/src/components/clientPage/comments.tsx
@@ -0,0 +1,10 @@
+import { Input } from '@chakra-ui/react'
+
+function Comments() {
+    return (
+        <div>
+            <Input placeholder="Type here"/>
+        </div>
+    );
+}
+export default Comments
