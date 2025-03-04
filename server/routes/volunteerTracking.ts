53536d08f22f92293fdc114c1a3b3d85c79824d4
maithyy <maithyle265@yahoo.com>
2025-03-04 15:17:23 -0800

    better formatting

--- server/routes/volunteerTracking.ts
+++ server/routes/volunteerTracking.ts
@@ -17,35 +17,40 @@ volunteerRouter.get("/", async (req, res) => {
 });
 // Get all users under specific event Case 1b
 volunteerRouter.get("/:event", async (req, res) => {
-    try {
-        const { event } = req.params;
-      const users = await db.query(`SELECT * FROM volunteers WHERE event_type = $1 ORDER BY id ASC`, 
-        [event, 
-        ]);
-      res.status(200).json(keysToCamel(users));
-    } catch (err) {
-      res.status(400).send(err.message);
-    }
-  });
+  try {
+    const { event } = req.params;
+    const users = await db.query(
+      `SELECT * FROM volunteers WHERE event_type = $1 ORDER BY id ASC`,
+      [event]
+    );
+    res.status(200).json(keysToCamel(users));
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
 
 // Total number of hours
 volunteerRouter.get("/total-hours", async (req, res) => {
-    try {
-      const data = await db.query(`SELECT SUM(hours) AS total_hours FROM volunteers`);
-      res.status(200).json(keysToCamel(data[0]));
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
+  try {
+    const data = await db.query(
+      `SELECT SUM(hours) AS total_hours FROM volunteers`
+    );
+    res.status(200).json(keysToCamel(data[0]));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 //Total volunteers
 volunteerRouter.get("/total-volunteers", async (req, res) => {
-    try {
-      const data = await db.query(`SELECT COUNT(DISTINCT first_name, last_name, email) AS total_volunteers FROM volunteers`);
-      res.status(200).json(keysToCamel(data[0]));
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
-  
-  export default volunteerRouter;
+  try {
+    const data = await db.query(
+      `SELECT COUNT(DISTINCT first_name, last_name, email) AS total_volunteers FROM volunteers`
+    );
+    res.status(200).json(keysToCamel(data[0]));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+export default volunteerRouter;
