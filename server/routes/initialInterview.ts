aa5bd36b2ff99b6ccd64de0fa4536ac6f2b2a834
Isaac Nguyen <58119262+IsaacNguyen@users.noreply.github.com>
2025-03-08 19:07:37 -0800

    Initial Screener and Comment Form  (#102)
    * feat: table and filter/search ready
    
    * made the form and hooked it up with your stuff
    
    * fix: fixed alignment and cm unique names
    
    * fields are FINALLY pre-populated w/ data
    
    * i hate patch requests
    
    * can gd actually save changes to the forms
    
    * It's getting toasty in here :)
    
    * done.
    
    * made requested changes: removed comments
    
    ---------
    
    Co-authored-by: Aditya Vemuri <avemuri2@uci.edu>

--- server/routes/initialInterview.ts
+++ server/routes/initialInterview.ts
@@ -5,6 +5,60 @@ import { db } from "../db/db-pgp";
 
 export const initialInterviewRouter = Router();
 
+//getting the data needed for the list of initial screeners
+initialInterviewRouter.get(
+  "/initial-interview-table-data",
+  async (req, res) => {
+    try {
+      const { search, page, filter } = req.query;
+      const stringSearch = "'%" + String(search) + "%'";
+
+      let queryStr = `
+      SELECT 
+        i.name AS client_name, 
+        i.social_worker_office_location, 
+        cm.first_name AS cm_first_name, 
+        cm.last_name AS cm_last_name, 
+        i.phone_number, 
+        i.email, 
+        i.date,
+        i.client_id
+      FROM initial_interview i
+      INNER JOIN screener_comment AS s ON i.id = s.initial_interview_id
+      INNER JOIN case_managers AS cm ON s.cm_id = cm.id
+    `;
+
+      if (search) {
+        queryStr += ` 
+        AND (
+          i.name::TEXT ILIKE ${stringSearch}
+          OR i.social_worker_office_location::TEXT ILIKE ${stringSearch}
+          OR cm.first_name::TEXT ILIKE ${stringSearch}
+          OR cm.last_name::TEXT ILIKE ${stringSearch}
+          OR i.phone_number::TEXT ILIKE ${stringSearch}
+          OR i.email::TEXT ILIKE ${stringSearch}
+          OR i.date::TEXT ILIKE ${stringSearch}
+        )`;
+      }
+
+      if (filter) {
+        queryStr += ` AND ${filter}`;
+      }
+
+      queryStr += " ORDER BY i.id ASC";
+
+      if (page) {
+        queryStr += ` LIMIT ${page}`;
+      }
+
+      const data = await db.query(queryStr);
+      res.status(200).json(keysToCamel(data));
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+);
+
 initialInterviewRouter.get("/", async (req, res) => {
   try {
     const data = await db.query(`SELECT * FROM initial_interview;`);
@@ -15,19 +69,82 @@ initialInterviewRouter.get("/", async (req, res) => {
   }
 });
 
-initialInterviewRouter.get("/:id", async (req, res) => {
+// Getting only comment form data with a client ID
+initialInterviewRouter.get("/commentForm/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const data = await db.query(`SELECT * FROM initial_interview WHERE client_id = $1;`, [id]);
+    const data = await db.query(`      
+      SELECT 
+        i.name AS client_name,  
+        cm.first_name AS cm_first_name, 
+        cm.last_name AS cm_last_name,
+        i.id AS initialId,
+        i.applicant_type, 
+        s.willingness,
+        s.employability, 
+        s.attitude, 
+        s.length_of_sobriety,
+        s.completed_tx,
+        s.drug_test_results,
+        s.homeless_episode_one,
+        s.homeless_episode_two,
+        s.homeless_episode_three,
+        s.disabling_condition,
+        s.employed,
+        s.driver_license,
+        s.num_of_children,
+        s.children_in_custody,
+        s.last_city_perm_residence,
+        s.decision,
+        s.additional_comments,
+        s.id
+      FROM initial_interview i
+      INNER JOIN screener_comment AS s ON i.id = s.initial_interview_id
+      INNER JOIN case_managers AS cm ON s.cm_id = cm.id
+      WHERE i.client_id = ${id};
+    `);
     res.status(200).json(keysToCamel(data));
   } catch (err) {
     res.status(500).send(err.message);
   }
-
 });
+
+initialInterviewRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await db.query(
+      `SELECT * FROM initial_interview WHERE client_id = $1;`,
+      [id]
+    );
+    res.status(200).json(keysToCamel(data));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+initialInterviewRouter.patch("/app-status/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    const query = `UPDATE initial_interview SET applicant_type = '${updates.applicant_type}' WHERE id = ${id} RETURNING *;`;
+    const result = await db.query(query);
+
+    res.status(200).json({
+      message: "Client updated successfully",
+      result: result,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 initialInterviewRouter.post("/", async (req, res) => {
-  try{
-    const {applicantType,
+  try {
+    const {
+      applicantType,
       name,
       age,
       date,
@@ -92,9 +209,10 @@ initialInterviewRouter.post("/", async (req, res) => {
       futurePlansGoals,
       lastPermanentResidenceHouseholdComposition,
       whyNoLongerAtLastResidence,
-      whatCouldPreventHomeless} = req.body;
+      whatCouldPreventHomeless,
+    } = req.body;
 
-      const query = `INSERT INTO initial_interview(applicantType, name, age, date, phoneNumber, maritalStatus, dateOfBirth, email, ssnLastFour, ethnicity, veteran, disabled, currentAddress, lastPermAddress, reasonForLeavingPermAddress, whereResideLastNight, currentlyHomeless, eventLeadingToHomelessness, howLongExperiencingHomelessness, prevAppliedToCch, whenPrevAppliedToCch, prevInCch, whenPrevInCch, childName, childDOB, city, custodyOfChild, fatherName, nameSchoolChildrenAttend, cityOfSchool, howHearAboutCch, programsBeenInBefore, monthlyIncome, sourcesOfIncome,
+    const query = `INSERT INTO initial_interview(applicantType, name, age, date, phoneNumber, maritalStatus, dateOfBirth, email, ssnLastFour, ethnicity, veteran, disabled, currentAddress, lastPermAddress, reasonForLeavingPermAddress, whereResideLastNight, currentlyHomeless, eventLeadingToHomelessness, howLongExperiencingHomelessness, prevAppliedToCch, whenPrevAppliedToCch, prevInCch, whenPrevInCch, childName, childDOB, city, custodyOfChild, fatherName, nameSchoolChildrenAttend, cityOfSchool, howHearAboutCch, programsBeenInBefore, monthlyIncome, sourcesOfIncome,
 monthlyBills, currentlyEmployed, lastEmployer, lastEmployedDate, childrenAge, educationHistory, transportation, legalResident, medical, medicalCity, medicalInsurance, medications, domesticViolenceHistory, socialWorker, socialWorkerTelephone, socialWorkerOfficeLocation, lengthOfSobriety, lastDrugUse, lastAlcoholUse, timeUsingDrugsAlcohol, beenConvicted, convictedReasonAndTime, presentWarrantExist, warrantCounty, probationParoleOfficer, probationParoleOfficerTelephone,
 personalReferences, personalReferenceTelephone, futurePlansGoals, lastPermanentResidenceHouseholdComposition, whyNoLongerAtLastResidence, whatCouldPreventHomeless
 ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34,
@@ -102,77 +220,75 @@ $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, 
 $65, $66, $67
 )`;
 
-  const res = await db.query(query, [applicantType,
-    name,
-    age,
-    date,
-    phoneNumber,
-    maritalStatus,
-    dateOfBirth,
-    email,
-    ssnLastFour,
-    ethnicity,
-    veteran,
-    disabled,
-    currentAddress,
-    lastPermAddress,
-    reasonForLeavingPermAddress,
-    whereResideLastNight,
-    currentlyHomeless,
-    eventLeadingToHomelessness,
-    howLongExperiencingHomelessness,
-    prevAppliedToCch,
-    whenPrevAppliedToCch,
-    prevInCch,
-    whenPrevInCch,
-    childName,
-    childDOB,
-    city,
-    custodyOfChild,
-    fatherName,
-    nameSchoolChildrenAttend,
-    cityOfSchool,
-    howHearAboutCch,
-    programsBeenInBefore,
-    monthlyIncome,
-    sourcesOfIncome,
-    monthlyBills,
-    currentlyEmployed,
-    lastEmployer,
-    lastEmployedDate,
-    childrenAge,
-    educationHistory,
-    transportation,
-    legalResident,
-    medical,
-    medicalCity,
-    medicalInsurance,
-    medications,
-    domesticViolenceHistory,
-    socialWorker,
-    socialWorkerTelephone,
-    socialWorkerOfficeLocation,
-    lengthOfSobriety,
-    lastDrugUse,
-    lastAlcoholUse,
-    timeUsingDrugsAlcohol,
-    beenConvicted,
-    convictedReasonAndTime,
-    presentWarrantExist,
-    warrantCounty,
-    probationParoleOfficer,
-    probationParoleOfficerTelephone,
-    personalReferences,
-    personalReferenceTelephone,
-    futurePlansGoals,
-    lastPermanentResidenceHouseholdComposition,
-    whyNoLongerAtLastResidence,
-    whatCouldPreventHomeless
-
-  ]);
-
-  } catch(err) {
+    const res = await db.query(query, [
+      applicantType,
+      name,
+      age,
+      date,
+      phoneNumber,
+      maritalStatus,
+      dateOfBirth,
+      email,
+      ssnLastFour,
+      ethnicity,
+      veteran,
+      disabled,
+      currentAddress,
+      lastPermAddress,
+      reasonForLeavingPermAddress,
+      whereResideLastNight,
+      currentlyHomeless,
+      eventLeadingToHomelessness,
+      howLongExperiencingHomelessness,
+      prevAppliedToCch,
+      whenPrevAppliedToCch,
+      prevInCch,
+      whenPrevInCch,
+      childName,
+      childDOB,
+      city,
+      custodyOfChild,
+      fatherName,
+      nameSchoolChildrenAttend,
+      cityOfSchool,
+      howHearAboutCch,
+      programsBeenInBefore,
+      monthlyIncome,
+      sourcesOfIncome,
+      monthlyBills,
+      currentlyEmployed,
+      lastEmployer,
+      lastEmployedDate,
+      childrenAge,
+      educationHistory,
+      transportation,
+      legalResident,
+      medical,
+      medicalCity,
+      medicalInsurance,
+      medications,
+      domesticViolenceHistory,
+      socialWorker,
+      socialWorkerTelephone,
+      socialWorkerOfficeLocation,
+      lengthOfSobriety,
+      lastDrugUse,
+      lastAlcoholUse,
+      timeUsingDrugsAlcohol,
+      beenConvicted,
+      convictedReasonAndTime,
+      presentWarrantExist,
+      warrantCounty,
+      probationParoleOfficer,
+      probationParoleOfficerTelephone,
+      personalReferences,
+      personalReferenceTelephone,
+      futurePlansGoals,
+      lastPermanentResidenceHouseholdComposition,
+      whyNoLongerAtLastResidence,
+      whatCouldPreventHomeless,
+    ]);
+  } catch (err) {
     res.status(500).send(err.message);
   }
-})
-
+});
