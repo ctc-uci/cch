73b448e6df51c7d34f899267c14cdf6f070035d7
SoulSen <talukdershowmen@gmail.com>
2025-04-13 23:45:30 -0700

    implement features for form preview

---
+++ client/src/components/formsHub/DataFormatter.tsx
@@ -0,0 +1,141 @@
+export const interviewScreenerKeyLabelMap: Record<string, string> = {
+  applicantType: "Applicant Type",
+  name: "Name",
+  age: "Age",
+  date: "Date",
+  phoneNumber: "Phone Number",
+  maritalStatus: "Marital Status",
+  dateOfBirth: "Date of Birth",
+  email: "Email",
+  ssnLastFour: "SSN (Last Four Digits)",
+  ethnicity: "Ethnicity",
+  veteran: "Veteran",
+  disabled: "Disabled",
+  currentAddress: "Current Address",
+  lastPermAddress: "Last Permanent Address",
+  reasonForLeavingPermAddress: "Reason for Leaving Permanent Address",
+  whereResideLastNight: "Where Did You Reside Last Night",
+  currentlyHomeless: "Currently Homeless",
+  eventLeadingToHomelessness: "Event Leading to Homelessness",
+  howLongExperiencingHomelessness: "Duration of Homelessness",
+  prevAppliedToCch: "Previously Applied to CCH",
+  whenPrevAppliedToCch: "When Previously Applied to CCH",
+  prevInCch: "Previously in CCH",
+  whenPrevInCch: "When Previously in CCH",
+  childName: "Child's Name",
+  childDob: "Child's Date of Birth",
+  custodyOfChild: "Custody of Child",
+  fatherName: "Father's Name",
+  nameSchoolChildrenAttend: "Name of School Attended by Child(ren)",
+  cityOfSchool: "City of School",
+  howHearAboutCch: "How Did You Hear About CCH",
+  programsBeenInBefore: "Programs Participated In Previously",
+  monthlyIncome: "Monthly Income",
+  sourcesOfIncome: "Sources of Income",
+  monthlyBills: "Monthly Bills",
+  currentlyEmployed: "Currently Employed",
+  lastEmployer: "Last Employer",
+  lastEmployedDate: "Last Employment Date",
+  educationHistory: "Education History",
+  transportation: "Transportation",
+  legalResident: "Legal Resident",
+  medical: "Medical Coverage",
+  medicalCity: "City of Medical Coverage",
+  medicalInsurance: "Medical Insurance Provider",
+  medications: "Medications",
+  domesticViolenceHistory: "Domestic Violence History",
+  socialWorker: "Social Worker",
+  socialWorkerTelephone: "Social Worker Telephone",
+  socialWorkerOfficeLocation: "Social Worker Office Location",
+  lengthOfSobriety: "Length of Sobriety",
+  lastDrugUse: "Last Drug Use",
+  lastAlcoholUse: "Last Alcohol Use",
+  timeUsingDrugsAlcohol: "Time Using Drugs/Alcohol",
+  beenConvicted: "Been Convicted",
+  convictedReasonAndTime: "Conviction Reason and Time",
+  presentWarrantExist: "Present Warrant Exists",
+  warrantCounty: "Warrant County",
+  probationParoleOfficer: "Probation/Parole Officer",
+  probationParoleOfficerTelephone: "Probation/Parole Officer Telephone",
+  personalReferences: "Personal Reference Name",
+  personalReferenceTelephone: "Personal Reference Telephone",
+  futurePlansGoals: "Future Plans and Goals",
+  lastPermanentResidenceHouseholdComposition: "Previous Household Composition",
+  whyNoLongerAtLastResidence: "Reason for Leaving Last Residence",
+  whatCouldPreventHomeless: "What Could Prevent Homelessness",
+};
+
+export const frontDeskKeyLabelMap: Record<string, string> = {
+  date: "Date",
+  totalOfficeVisits: "Total Office Visits",
+  totalCalls: "Total # of Calls",
+  totalUnduplicatedCalls: "Total # of Unduplicated Calls",
+  totalVisitsHbDonationsRoom: "Total Visits to HB Donations Room",
+  totalServedHbDonationsRoom: "Total People Served in HB Donations Room",
+  totalVisitsHbPantry: "Total Visits to HB Pantry",
+  totalServedHbPantry: "Total People Served in HB Pantry",
+  totalVisitsPlacentiaPantry: "Total Visits to Placentia Pantry",
+  totalServedPlacentiaPantry: "Total People Served in Placentia Pantry",
+  totalVisitsPlacentiaNeighborhood: "Total Visits to Placentia Neighborhood Room",
+  totalServedPlacentiaNeighborhood: "Total People Served in Placentia Neighborhood Room",
+  numberOfPeople: "Number of People"
+};
+
+export const caseManagerKeyLabelMap: Record<string, string> = {
+  role: "Role",
+  firstName: "First Name",
+  lastName: "Last Name",
+  phoneNumber: "Phone Number",
+  email: "Email"
+};
+
+const config: Record<
+  string,
+  { labelMap: Record<string, string>; excludeKeys: string[] }
+> = {
+  "Initial Screeners": {
+    labelMap: interviewScreenerKeyLabelMap,
+    excludeKeys: ["id", "clientId"],
+  },
+  "Front Desk Monthly Statistics": {
+    labelMap: frontDeskKeyLabelMap,
+    excludeKeys: ["id"],
+  },
+  "Case Manager Monthly Statistics": {
+    labelMap: caseManagerKeyLabelMap,
+    excludeKeys: ["id"],
+  },
+};
+
+
+export const formatDataWithLabels = (
+  rawData: Record<string, any>,
+  title: string
+): Record<string, any> => {
+  const formatted: Record<string, any> = {};
+
+  const section = config[title];
+  if (!section) return rawData;
+
+  for (const [key, value] of Object.entries(rawData)) {
+    if (section.excludeKeys.includes(key)) continue;
+
+    const readableKey = section.labelMap[key] ?? key;
+    formatted[readableKey] = value;
+  }
+
+  return formatted;
+};
+
+export const getKeyByValue = (
+  value: string,
+  title: string
+): string | undefined | null => {
+
+  const section = config[title];
+
+  if (!section) return null;
+
+  return Object.entries(section.labelMap).find(([_, val]) => val === value)?.[0];
+};
+
--- client/src/components/formsHub/FormPreview.tsx
+++ client/src/components/formsHub/FormPreview.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import { ChevronRightIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -8,7 +9,9 @@ import {
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
-  Flex,
+  HStack,
+  IconButton,
+  Input,
   Table,
   Tbody,
   Td,
@@ -19,7 +22,9 @@ import {
 } from "@chakra-ui/react";
 
 import { useBackendContext } from "../../contexts/hooks/useBackendContext";
+import { formatDataWithLabels, getKeyByValue } from "./DataFormatter.tsx";
 import { FormItem } from "./formsTable";
+import {downloadCSV} from "../../utils/downloadCSV.ts";
 
 export const FormPreview = ({
   formItem,
@@ -33,10 +38,13 @@ export const FormPreview = ({
   const { backend } = useBackendContext();
 
   const [formData, setFormData] = useState({});
+  const [newFormattedFormData, setNewFormattedFormData] = useState({});
+  const [formattedFormData, setFormattedFormData] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
 
   const userId = formItem.id;
 
-  const titleToEndpoint: Record<FormItem["title"], string> = {
+  const titleToGetEndpoint: Record<FormItem["title"], string> = {
     "Initial Screeners": `/initialInterview/get-interview/${userId}`,
     "Intake Statistics": "",
     "Front Desk Monthly Statistics": `/frontDesk/get-stat/${userId}`,
@@ -45,9 +53,11 @@ export const FormPreview = ({
 
   useEffect(() => {
     const getData = async () => {
-      const endpoint = titleToEndpoint[formItem.title];
+      if (isOpen) return;
 
-      if (endpoint == "") {
+      const endpoint = titleToGetEndpoint[formItem.title];
+
+      if (endpoint === "") {
         setFormData({});
 
         return;
@@ -55,43 +65,115 @@ export const FormPreview = ({
 
       try {
         const response = await backend.get(`${endpoint}`);
-        const data = response.data;
+        const data = formatDataWithLabels(response.data[0], formItem.title);
 
-        setFormData(data);
+        setFormData(response.data[0]);
+
+        setFormattedFormData(data);
+        setNewFormattedFormData(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     getData();
-  }, [backend, formItem]);
+  }, [isOpen, backend, formItem, titleToGetEndpoint]);
+
+  const handleSaveForm = async () => {
+    // do the put requests and error handling
+  }
+
+  const isDate = (value: any) => {
+    return (
+      Object.prototype.toString.call(value) === "[object Date]" ||
+      (typeof value === "string" && !isNaN(Date.parse(value)))
+    );
+  }
 
   return (
     <Drawer
       isOpen={isOpen}
       placement="right"
-      onClose={onClose}
+      onClose={() => {
+        onClose();
+        setIsEditing(false);
+      }}
       size="xl"
     >
       <DrawerOverlay />
       <DrawerContent>
         <DrawerHeader borderBottomWidth="1px">
-          <Flex
-            justify="space-between"
-            align="left"
-          >
+          <HStack spacing={2}>
+            <IconButton
+              as={ChevronRightIcon}
+              color="blue.500"
+              aria-label="Close drawer"
+              onClick={() => {
+                onClose();
+                setIsEditing(false);
+              }}
+              variant="ghost"
+              size="sm"
+            />
             <Text fontWeight="bold">Form Preview</Text>
-            <Text>{formItem.title}</Text>
-          </Flex>
+            <Text color="gray.600">
+              {formItem.name} - {formItem.title} {formItem.date}
+            </Text>
+          </HStack>
         </DrawerHeader>
 
         <DrawerBody>
-          <Button
-            colorScheme="blue"
-            mb={4}
-          >
-            Edit Form
-          </Button>
+          {!isEditing ? (
+            <HStack>
+              <Button
+                colorScheme="blue"
+                onClick={() => setIsEditing(true)}
+              >
+                Edit Form
+              </Button>
+
+              <Button
+                colorScheme="blue"
+                float="right"
+                onClick={() => {
+                  const headers = ["Questions", "Answer"];
+                  const data = Object.entries(newFormattedFormData).map(([key, value]) => ({
+                    Questions: key,
+                    Answer: value
+                  }));
+                  downloadCSV(headers, data, "form.csv");
+                }}
+              >
+                Export Form
+              </Button>
+            </HStack>
+          ) : (
+            <HStack
+              spacing={3}
+              justify="flex-end"
+              mt={6}
+            >
+              <Button
+                variant="ghost"
+                onClick={() => {
+                  setNewFormattedFormData(formattedFormData);
+                  setIsEditing(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                colorScheme="blue"
+                onClick={() => {
+                  setFormattedFormData(newFormattedFormData);
+                  handleSaveForm();
+                  setIsEditing(false);
+                }}
+              >
+                Save
+              </Button>
+            </HStack>
+          )}
 
           <Table variant="simple">
             <Thead>
@@ -100,16 +182,48 @@ export const FormPreview = ({
                 <Th>ANSWER</Th>
               </Tr>
             </Thead>
-            <Tbody></Tbody>
-          </Table>
+            <Tbody>
+              {Object.keys(newFormattedFormData).length > 0 &&
+                Object.entries(newFormattedFormData).map(([key, value]) => (
+                  <Tr key={key}>
+                    <Td>{key}</Td>
+                    <Td>
+                      {!isEditing ? (
+                        <>{value}</>
+                      ) : (
+                        <Input
+                          value={value ?? ""}
+                          onChange={(event) => {
+                            const originalValue = formattedFormData[key];
 
-          <Button
-            mt={6}
-            colorScheme="blue"
-            float="right"
-          >
-            Export Form
-          </Button>
+                            // event.target.value is a string
+                            // it needs to be casted to match the type in the actual form
+                            let newValue: any = event.target.value;
+
+                            if (typeof originalValue === "number") {
+                              newValue = Number(newValue);
+                            } else if (typeof originalValue === "boolean") {
+                              newValue = newValue.toLowerCase() === "true";
+                            } else if (isDate(originalValue)) {
+                              const parsedDate = new Date(newValue);
+
+                              if (!isNaN(parsedDate.getTime())) {
+                                newValue = parsedDate.toISOString();
+                              }
+                            }
+
+                            setNewFormattedFormData((prev) => ({
+                              ...prev,
+                              [key]: newValue,
+                            }));
+                          }}
+                        ></Input>
+                      )}
+                    </Td>
+                  </Tr>
+                ))}
+            </Tbody>
+          </Table>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
