2ec26b5204de57934e29fee6a4c8ec08239ac273
Brendan Lieu <brendanlieu05@gmail.com>
2026-01-13 14:54:36 -0800

    fix: env prod variable

--- server/common/transporter.ts
+++ server/common/transporter.ts
@@ -16,7 +16,7 @@ const pass =
 const emailSender =
   process.env.NODE_ENV === "development"
     ? `${process.env.DEV_EMAIL_FIRSTNAME} ${process.env.DEV_EMAIL_LASTNAME}`
-    : `${process.env.PROD_EMAIL_FIRSTNAMS} ${process.env.DEV_EMAIL_LASTNAME}`;
+    : `${process.env.PROD_EMAIL_FIRSTNAME} ${process.env.PROD_EMAIL_LASTNAME}`;
 const sendEmail = 
   process.env.NODE_ENV === "development"
     ? `${process.env.DEV_USERNAME}`
